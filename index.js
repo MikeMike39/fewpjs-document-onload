7e5e12575231823a2a7418409ff84606341f11e6
MikeMike39 <Mfonseca294@gmail.com>
2021-04-04 11:03:59 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+// Add a Event listener waiting for the DOM update
+document.addEventListener( "DOMContentLoaded", function () {
+  updateDOM()
+} );
+
+// Should change the text on the page when DOM content has loaded
+function updateDOM() {
+  document.getElementById( "text" )
+    .innerHTML = "This is really cool!";
+}
